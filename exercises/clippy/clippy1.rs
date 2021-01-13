9c8c95fcdfd980f004e7ab3d61f87373b6d0190a
waymobetta <jbroethke@gmail.com>
2021-01-13 11:41:48 -0800

    completed

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,14 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+    
+    let error_margin = f64::EPSILON;
+ 
+    // get absolute value of diff
+    if (x - y).abs() < error_margin {
         println!("Success!");
     }
 }
