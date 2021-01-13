dd5c60c73ad174d006621e7d92c5fe25f4aa1b30
waymobetta <jbroethke@gmail.com>
2021-01-13 01:59:35 -0800

    completed

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,9 @@
 //
 // Execute `rustlings hint box1` for hints :)
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -32,12 +30,16 @@ fn main() {
     );
 }
 
+use crate::List::{Cons,Nil};
+
 pub fn create_empty_list() -> List {
-    unimplemented!()
+    let list = List::Nil;
+    list
 }
 
 pub fn create_non_empty_list() -> List {
-    unimplemented!()
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    list
 }
 
 #[cfg(test)]
