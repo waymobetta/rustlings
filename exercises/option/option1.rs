7c213a74de5a55098a2021d44186d4140b302c01
waymobetta <jbroethke@gmail.com>
2021-01-12 17:43:31 -0800

    completed

--- exercises/option/option1.rs
+++ exercises/option/option1.rs
@@ -1,22 +1,23 @@
 // option1.rs
 // Make me compile! Execute `rustlings hint option1` for hints
 
-// I AM NOT DONE
-
 // you can modify anything EXCEPT for this function's sig
 fn print_number(maybe_number: Option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
 }
 
 fn main() {
-    print_number(13);
-    print_number(99);
+    print_number(Some(13)); 
+    print_number(Some(99));
+    
+    // leverage default crate to init a fixed-length array of u16 up to 32 items
+    // let mut numbers: [Option<u16>; 5] = Default::default();
+    
+    // or use None
+    let mut numbers: [Option<u16>; 5] = [None; 5];
 
-    let mut numbers: [Option<u16>; 5];
     for iter in 0..5 {
-        let number_to_add: u16 = {
-            ((iter * 1235) + 2) / (4 * 16)
-        };
+        let number_to_add: Option<u16> = Some(((iter * 1235) + 2) / (4 * 16));
 
         numbers[iter as usize] = number_to_add;
     }
